1f54c4b871e6825564d7981d42a56d3683b65b01
take8 <37664118+take8@users.noreply.github.com>
2024-08-03 02:38:19 +0900

    Update: add inbound rule

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,13 @@ resource "aws_security_group" "example_ec2_sg" {
     Name = "example-ec2-sg"
   }
 }
+
+# 外部からHTTP 80ポートへの通信を許可
+resource "aws_security_group_rule" "example_ec2_in_http" {
+  security_group_id = aws_security_group.example_ec2_sg.id
+  type = "ingress"
+  protocol = "tcp"
+  from_port = 80
+  to_port = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
