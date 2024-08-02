4d06a63582a79fbd07d0dc9e8dc5e1d55d9dd3b0
take8 <37664118+take8@users.noreply.github.com>
2024-08-03 01:59:40 +0900

    Update: add subnet

--- network.tf
+++ network.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "example_vpc" {
     Name = "example-vpc"
   }
 }
+
+resource "aws_subnet" "example_subnet_a" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = "example-subnet-a"
+}
